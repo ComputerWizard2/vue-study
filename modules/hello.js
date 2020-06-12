b63cbcd328af7d69f8507e794d02b3dc839ed663
computerwizard2 <1972201765@qq.com>
2020-06-13 06:16:19 +0800

    上传测试

---
+++ modules/hello.js
@@ -0,0 +1,13 @@
+//暴露一个方法
+exports.sayHi1 =function () {
+document.write("<h1>我想学Vue1</h1>");
+
+}
+exports.sayHi2 =function () {
+    document.write("<h1>我想学Vue2</h1>");
+
+}
+exports.sayHi3 =function () {
+    document.write("<h1>我想学Vue3</h1>");
+
+}
---
+++ modules/main.js
@@ -0,0 +1,6 @@
+//引入这个js 模块
+var hello=require("./hello");
+
+hello.sayHi1();
+hello.sayHi2();
+hello.sayHi3();
